a1048f904d9b5c7b7660e6acff39ccb8452d9c3c
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-01 00:21:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,4 +5,10 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = 0
   name_hash.each do |key,value|
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
